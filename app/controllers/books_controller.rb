09718e25217b334e62272c0dc65d8057c73e682b
chonasan <tk24120824@gmail.com>
2021-01-08 16:11:53 +0000

    booker

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
+  def books
+  end
+  
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'homes#top'
-end
+  get "/" => "books#top"
+  get "/books" => "books#index"
+  
+  post 'books' => 'todolists#create'
+  
+  resources :blogs
+ end
